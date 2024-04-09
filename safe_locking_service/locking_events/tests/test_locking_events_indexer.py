5dd57dd7fd01fcc0e1e37238b7adf04cf745ddaa
Felipe Alvarado <felipe@safe.global>
2024-04-09 18:08:55 +0200

    Add tests for withdrawn event and event decoding

--- safe_locking_service/locking_events/tests/test_locking_events_indexer.py
+++ safe_locking_service/locking_events/tests/test_locking_events_indexer.py
@@ -8,17 +8,43 @@ from ..indexers.safe_locking_events_indexer import (
     SafeLockingEventsIndexer,
     get_safe_locking_event_indexer,
 )
-from ..models import EthereumTx, LockEvent, StatusEventsIndexer, UnlockEvent
-from .utils import erc20_approve, locking_contract_lock, locking_contract_unlock
+from ..models import (
+    EthereumTx,
+    LockEvent,
+    StatusEventsIndexer,
+    UnlockEvent,
+    WithdrawnEvent,
+)
+from .mocks.mocks_locking_events_indexer import (
+    invalid_key_event,
+    invalid_lock_event,
+    invalid_unlock_event,
+    invalid_withdrawn_event,
+    valid_lock_event,
+    valid_unlock_event,
+    valid_withdrawn_event,
+)
+from .utils import (
+    erc20_approve,
+    increment_chain_time,
+    locking_contract_lock,
+    locking_contract_unlock,
+    locking_contract_withdraw,
+)
 
 
 class TestLockingEventsIndexer(EthereumTestCaseMixin, TestCase):
+    cooldown_period = 2
+
     def setUp(self) -> None:
         account = self.ethereum_test_account
         amount = 10000000
         self.erc20_contract = self.deploy_example_erc20(amount, account.address)
         self.locking_contract = deploy_locking_contract(
-            self.ethereum_client, account, self.erc20_contract.address
+            self.ethereum_client,
+            account,
+            self.erc20_contract.address,
+            self.cooldown_period,
         )
         self.locking_contract_address = self.locking_contract.address
         self.assertIsNotNone(self.locking_contract.address)
@@ -97,3 +123,96 @@ class TestLockingEventsIndexer(EthereumTestCaseMixin, TestCase):
             )["total"],
             100,
         )
+
+    def test_index_withdrawn_events(self):
+        account = self.ethereum_test_account
+        lock_amount = 100
+        erc20_approve(
+            self.ethereum_client.w3,
+            account,
+            self.erc20_contract,
+            self.locking_contract.address,
+            lock_amount,
+        )
+        locking_events_indexer = SafeLockingEventsIndexer(self.locking_contract_address)
+        locking_events_indexer.index_until_last_chain_block()
+        self.assertEqual(EthereumTx.objects.count(), 0)
+        self.assertEqual(UnlockEvent.objects.count(), 0)
+        locking_contract_lock(
+            self.ethereum_client.w3, account, self.locking_contract, 100
+        )
+        for i in range(0, 10):
+            locking_contract_unlock(
+                self.ethereum_client.w3, account, self.locking_contract, 10
+            )
+        locking_events_indexer.index_until_last_chain_block()
+        increment_chain_time(self.ethereum_client.w3, self.cooldown_period)
+        locking_contract_withdraw(
+            self.ethereum_client.w3, account, self.locking_contract, 5
+        )
+        locking_events_indexer.index_until_last_chain_block()
+        # 1 Lock, 10 Unlock and 1 Withdrawn
+        self.assertEqual(EthereumTx.objects.count(), 12)
+        self.assertEqual(LockEvent.objects.filter(holder=account.address).count(), 1)
+        self.assertEqual(UnlockEvent.objects.filter(holder=account.address).count(), 10)
+        # 5 withdrawn with amount of 10 by previous unlock event.
+        self.assertEqual(
+            WithdrawnEvent.objects.filter(holder=account.address).count(), 5
+        )
+
+        self.assertEqual(
+            WithdrawnEvent.objects.filter(holder=account.address).aggregate(
+                total=Sum("amount")
+            )["total"],
+            50,
+        )
+
+    def test_event_decoding(self):
+        locking_events_indexer = SafeLockingEventsIndexer(self.locking_contract_address)
+
+        self.assertRaises(
+            KeyError, locking_events_indexer.decode_event, invalid_key_event
+        )
+
+        data_lock_event = locking_events_indexer.decode_event(valid_lock_event)
+        self.assertEqual(data_lock_event.get("event"), "Locked")
+        self.assertEqual(
+            data_lock_event.get("args").get("holder"),
+            "0x22D491bde2303f2F43325b2108d26F1EaBA1E32A",
+        )
+        self.assertEqual(data_lock_event.get("args").get("amount"), 100)
+
+        invalid_data_lock_event = locking_events_indexer.decode_event(
+            invalid_lock_event
+        )
+        self.assertIsNone(invalid_data_lock_event)
+
+        data_unlock_event = locking_events_indexer.decode_event(valid_unlock_event)
+        self.assertEqual(data_unlock_event.get("event"), "Unlocked")
+        self.assertEqual(
+            data_unlock_event.get("args").get("holder"),
+            "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b",
+        )
+        self.assertEqual(data_unlock_event.get("args").get("index"), 9)
+        self.assertEqual(data_unlock_event.get("args").get("amount"), 10)
+
+        invalid_data_unlock_event = locking_events_indexer.decode_event(
+            invalid_unlock_event
+        )
+        self.assertIsNone(invalid_data_unlock_event)
+
+        data_withdrawn_event = locking_events_indexer.decode_event(
+            valid_withdrawn_event
+        )
+        self.assertEqual(data_withdrawn_event.get("event"), "Withdrawn")
+        self.assertEqual(
+            data_withdrawn_event.get("args").get("holder"),
+            "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b",
+        )
+        self.assertEqual(data_withdrawn_event.get("args").get("index"), 2)
+        self.assertEqual(data_withdrawn_event.get("args").get("amount"), 10)
+
+        invalid_data_withdrawn_event = locking_events_indexer.decode_event(
+            invalid_withdrawn_event
+        )
+        self.assertIsNone(invalid_data_withdrawn_event)
--- safe_locking_service/locking_events/tests/utils.py
+++ safe_locking_service/locking_events/tests/utils.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from django.utils import timezone
 
@@ -6,6 +6,7 @@ from eth_account.signers.local import LocalAccount
 from eth_typing import ChecksumAddress
 from web3 import Web3
 from web3.contract import Contract
+from web3.types import RPCEndpoint
 
 from .factories import LockEventFactory, UnlockEventFactory, WithdrawnEventFactory
 
@@ -59,6 +60,21 @@ def locking_contract_unlock(
     return tx_receipt
 
 
+def locking_contract_withdraw(
+    w3: Web3, signer_account: LocalAccount, locking_contract: Contract, max_unlocks: int
+):
+    tx_withdrawn = locking_contract.functions.withdraw(max_unlocks).build_transaction(
+        {
+            "from": signer_account.address,
+            "nonce": w3.eth.get_transaction_count(signer_account.address),
+        }
+    )
+    signed_tx = signer_account.sign_transaction(tx_withdrawn)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    return tx_receipt
+
+
 def add_sorted_events(
     address: ChecksumAddress,
     lock_amount: int,
@@ -87,3 +103,29 @@ def add_sorted_events(
     WithdrawnEventFactory(
         holder=address, amount=withdrawn_amount, timestamp=timezone.now()
     )
+
+
+def increment_chain_time(w3: Web3, increased_time: int) -> None:
+    """
+    Increase the time to some future point manually to simulate any test situation where you need to wait for some time.
+    Uses `evm_increaseTime` supported by Ganache and Hardhat.
+
+    :param w3: Ethereum client w3.
+    :param increased_time: Time in seconds to be advanced.
+    :return:
+    """
+    current_timestamp = w3.eth.get_block("latest").get("timestamp")
+    date = datetime.fromtimestamp(current_timestamp)
+    new_date = datetime(
+        date.year,
+        date.month,
+        date.day,
+        date.hour,
+        date.minute,
+        date.second + increased_time,
+    )
+
+    w3.provider.make_request(
+        RPCEndpoint("evm_increaseTime"), [int(new_date.timestamp()) - current_timestamp]
+    )
+    w3.provider.make_request(RPCEndpoint("evm_mine"), [])
