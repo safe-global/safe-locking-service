860e9c23344c91e62aec5dddec30e2c92aef6b14
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2024-05-14 14:11:20 +0200

    Add Period and Activity models
    - Adds a `Period` and `Activity` models.
    - A `Period` represents a period of time for a given `Campaign`. It can be identified via a `slug` which uses the time period to compute it and then can be used to identify the period externally.
    - An `Activity` represents the activity for a given `Period` for a given `address`

--- safe_locking_service/campaigns/admin.py
+++ safe_locking_service/campaigns/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
 
-from .models import Campaign
+from .models import Activity, Campaign, Period
 
 
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "start_date", "end_date")
     search_fields = ("name",)
-    fields = ("name", "description", "start_date", "end_date")
+    fields = (
+        "name",
+        "description",
+        "start_date",
+        "end_date",
+    )
+
+
+@admin.register(Period)
+class PeriodAdmin(admin.ModelAdmin):
+    list_display = (
+        "campaign",
+        "slug",
+        "start_date",
+        "end_date",
+    )
+    search_fields = ("campaign",)
+    fields = (
+        "campaign",
+        "start_date",
+        "end_date",
+    )
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = (
+        "period",
+        "address",
+        "total_points",
+        "boost",
+        "total_boosted_points",
+    )
+    search_fields = ("address",)
--- safe_locking_service/campaigns/models.py
+++ safe_locking_service/campaigns/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.text import slugify
+
+from gnosis.eth.django.models import EthereumAddressV2Field
 
 
 class Campaign(models.Model):
@@ -10,3 +13,49 @@ class Campaign(models.Model):
 
     def __str__(self):
         return f"Campaign {self.id} {self.name}"
+
+
+class Period(models.Model):
+    campaign = models.ForeignKey(
+        Campaign, on_delete=models.CASCADE, related_name="periods"
+    )
+    slug = models.SlugField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{self.start_date}-{self.end_date}")
+        super(Period, self).save(*args, **kwargs)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["campaign", "end_date"], name="unique_period"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.campaign} – Period {self.slug}"
+
+
+class Activity(models.Model):
+    period = models.ForeignKey(
+        Period, on_delete=models.CASCADE, related_name="activities"
+    )
+    address = EthereumAddressV2Field()
+    total_points = models.PositiveBigIntegerField()
+    boost = models.DecimalField(max_digits=15, decimal_places=8)
+    total_boosted_points = models.DecimalField(max_digits=15, decimal_places=8)
+
+    # TODO List of activity types
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["period", "address"], name="unique_activity"
+            )
+        ]
+
+    def __str__(self):
+        return f"Activity {self.period} {self.address}"
