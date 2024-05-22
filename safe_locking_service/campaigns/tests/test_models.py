b18f2452b4d49264622f54e5b24a55f7063a57d3
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2024-05-22 09:19:39 +0200

    Validate Campaign date ranges (#99)
    Validates that the date time ranges are valid – the start_date must be at the same time or before end_date.

--- safe_locking_service/campaigns/tests/test_models.py
+++ safe_locking_service/campaigns/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from django.db import DataError, IntegrityError
+from django.db import IntegrityError
 from django.test import TestCase
 
 from faker import Faker
@@ -19,15 +19,14 @@ class CampaignsTestCase(TestCase):
         self.assertEqual(str(campaign), f"Campaign {campaign.uuid} {campaign.name}")
 
     def test_campaign_with_null_name(self):
-        campaign = CampaignFactory.build(name=None)
-
-        self.assertRaises(IntegrityError, campaign.save)
+        with self.assertRaises(ValidationError):
+            CampaignFactory(name=None)
 
     def test_campaign_with_name_longer_than_50_characters(self):
         invalid_name = fake.pystr(min_chars=51, max_chars=255)
-        campaign = CampaignFactory.build(name=invalid_name)
 
-        self.assertRaises(DataError, campaign.save)
+        with self.assertRaises(ValidationError):
+            CampaignFactory(name=invalid_name)
 
     def test_campaign_with_null_description(self):
         campaign = CampaignFactory.build(description=None)
@@ -43,6 +42,18 @@ class CampaignsTestCase(TestCase):
         campaign = CampaignFactory.build(end_date=None)
 
         campaign.save()
+
+    def test_allows_same_dates(self):
+        date = fake.date_time()
+
+        CampaignFactory(end_date=date, start_date=date)
+
+    def test_does_not_allow_end_date_before_start_date(self):
+        start_date = fake.future_datetime()
+        end_date = fake.past_datetime()
+
+        with self.assertRaises(ValidationError):
+            CampaignFactory(end_date=end_date, start_date=start_date)
 
 
 class PeriodTestCase(TestCase):
