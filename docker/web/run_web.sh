90c195917a2f3156593dfbcbaf42272d884539f8
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2024-05-24 14:48:29 +0200

    wip

--- docker/web/run_web.sh
+++ docker/web/run_web.sh
@@ -2,11 +2,15 @@
 
 set -euo pipefail
 
-echo "==> $(date +%H:%M:%S) ==> Collecting statics... "
 DOCKER_SHARED_DIR=/nginx
+
+echo "==> $(date +%H:%M:%S) ==> Collecting statics... "
+python manage.py collectstatic --noinput
 rm -rf $DOCKER_SHARED_DIR/*
-# STATIC_ROOT=$DOCKER_SHARED_DIR/staticfiles python manage.py collectstatic --noinput &
 cp -r staticfiles/ $DOCKER_SHARED_DIR/
+
+echo "==> $(date +%H:%M:%S) ==> Migrating Django models..."
+python manage.py migrate --noinput
 
 echo "==> $(date +%H:%M:%S) ==> Running Gunicorn... "
 exec gunicorn --config gunicorn.conf.py --pythonpath "$PWD" -b unix:$DOCKER_SHARED_DIR/gunicorn.socket -b 0.0.0.0:8888 config.wsgi:application
